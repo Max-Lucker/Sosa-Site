90dfc48ee57b1eefcad6463cbfadd7285584f57c
Max Shevcenko <maxlucker1994@gmail.com>
2020-01-26 23:45:29 +0200

    main page

---
+++ src/app.js
@@ -0,0 +1,8 @@
+import 'airbnb-browser-shims';
+import 'datatables.net';
+import 'jquery';
+import 'popper.js';
+import 'bootstrap';
+
+import 'mdbootstrap/js/modules/bs-custom-file-input';
+import 'mdbootstrap/js/mdb';
